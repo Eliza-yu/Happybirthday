f8e282abdc2e85e8aa4d541afbe2096905ec0dde
Eliza-yu <129805058+Eliza-yu@users.noreply.github.com>
2026-04-22 15:19:35 +0200

    Add interactive buttons for dialogue and character changes

---
+++ script.js
@@ -0,0 +1,41 @@
+let yesBtn = document.getElementById("yesBtn");
+let noBtn = document.getElementById("noBtn");
+
+let dialogue = document.getElementById("dialogue");
+let character = document.getElementById("character");
+let background = document.getElementById("background");
+
+let song = document.getElementById("song");
+
+let noCount = 0;
+
+yesBtn.onclick = function(){
+
+    background.src = "bg2.png";
+    character.src = "character3.png";
+    dialogue.innerText = "Happy Birthday!!!";
+
+    song.play();
+
+    document.getElementById("choices").style.display = "none";
+}
+
+noBtn.onclick = function(){
+
+    noCount++;
+
+    if(noCount == 1){
+        character.src = "character2.png";
+        dialogue.innerText = "Liar.";
+
+        noBtn.style.transform = "scale(0.7)";
+    }
+
+    else if(noCount == 2){
+        character.src = "character4.png";
+        dialogue.innerText = "Yeah yeah.";
+
+        noBtn.style.display = "none";
+    }
+
+}
