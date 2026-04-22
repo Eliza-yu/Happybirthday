84e8a01a71cf2d469c0d225846a0acd34458364b
Eliza-yu <129805058+Eliza-yu@users.noreply.github.com>
2026-04-22 15:57:34 +0200

    Correct character image and implement confetti
    Fix character image filename and add confetti effect.

--- script.js
+++ script.js
@@ -12,10 +12,12 @@ let noCount = 0;
 yesBtn.onclick = function(){
 
     background.src = "bg2.png";
-    character.src = "character3.PNG";
+    character.src = "character3.png";
     dialogue.innerText = "Happy Birthday!!!";
 
     song.play();
+
+    drawConfetti();   // start infinite confetti
 
     document.getElementById("choices").style.display = "none";
 }
@@ -38,4 +40,42 @@ noBtn.onclick = function(){
         noBtn.style.display = "none";
     }
 
+}
+let canvas = document.getElementById("confetti");
+let ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 600;
+
+let confettiPieces = [];
+
+for(let i=0;i<150;i++){
+    confettiPieces.push({
+        x: Math.random()*canvas.width,
+        y: Math.random()*canvas.height,
+        size: Math.random()*6+4,
+        speed: Math.random()*3+2
+    });
+}
+
+function drawConfetti(){
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    confettiPieces.forEach(piece => {
+
+        ctx.fillStyle = `hsl(${Math.random()*360},70%,60%)`;
+
+        ctx.fillRect(piece.x,piece.y,piece.size,piece.size);
+
+        piece.y += piece.speed;
+
+        if(piece.y > canvas.height){
+            piece.y = -10;
+            piece.x = Math.random()*canvas.width;
+        }
+
+    });
+
+    requestAnimationFrame(drawConfetti);
 }
