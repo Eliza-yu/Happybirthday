52ead1e66afa9caf55b2eb1f10685d9ef7846d88
Eliza-yu <129805058+Eliza-yu@users.noreply.github.com>
2026-04-22 15:34:23 +0200

    Fix image file extensions to uppercase PNG

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let noCount = 0;
 yesBtn.onclick = function(){
 
     background.src = "bg2.png";
-    character.src = "character3.png";
+    character.src = "character3.PNG";
     dialogue.innerText = "Happy Birthday!!!";
 
     song.play();
@@ -25,14 +25,14 @@ noBtn.onclick = function(){
     noCount++;
 
     if(noCount == 1){
-        character.src = "character2.png";
+        character.src = "character2.PNG";
         dialogue.innerText = "Liar.";
 
         noBtn.style.transform = "scale(0.7)";
     }
 
     else if(noCount == 2){
-        character.src = "character4.png";
+        character.src = "character4.PNG";
         dialogue.innerText = "Yeah yeah.";
 
         noBtn.style.display = "none";
